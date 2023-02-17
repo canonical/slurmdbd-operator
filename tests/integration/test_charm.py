3b98068f450ce7df36897018a920a148be12d159
NucciTheBoss <jason.nucciarone@canonical.com>
2023-02-17 15:01:39 -0500

    enhance: Switch from percona to mysql database

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 SERIES = ["focal"]
 SLURMDBD = "slurmdbd"
 SLURMCTLD = "slurmctld"
-DATABASE = "percona-cluster"
+DATABASE = "mysql"
 
 
 @pytest.mark.abort_on_fail
@@ -63,14 +63,14 @@ async def test_build_and_deploy_against_edge(
             application_name=DATABASE,
             channel="edge",
             num_units=1,
-            series="bionic",
+            series="jammy",
         ),
     )
     # Attach resources to charms.
     await ops_test.juju("attach-resource", SLURMCTLD, f"etcd={slurmctld_res['etcd']}")
-    # Set integrations for charmed applications.
+    # Set relations for charmed applications.
     await ops_test.model.relate(f"{SLURMDBD}:{SLURMDBD}", f"{SLURMCTLD}:{SLURMDBD}")
-    await ops_test.model.relate(f"{SLURMDBD}:db", f"{DATABASE}:db")
+    await ops_test.model.relate(f"{SLURMDBD}:database", f"{DATABASE}:database")
     # Reduce the update status frequency to accelerate the triggering of deferred events.
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(apps=[SLURMDBD], status="active", timeout=1000)
